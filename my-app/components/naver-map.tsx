ff8431b164496ff77ce1c23453c5525b04caef62
weturnit42 <we_turn_it42@naver.com>
2025-01-09 04:17:51 +0900

    fix location obj bug with using both useState and useRef

--- my-app/components/naver-map.tsx
+++ my-app/components/naver-map.tsx
@@ -5,11 +5,19 @@ type Address = {
 };
 
 const Map: React.FC<Address> = ({ route }) => {
+  const testFlag = false;
+
   const [location, setLocation] = useState<{ lat: number; lng: number }>({ lat: 37.5739531, lng: 127.1940553 }); // 사용자 위치 상태
+  const locationRef = useRef(location);
+
   const mapRef = useRef<naver.maps.Map | null>(null);
   const markerRef = useRef<naver.maps.Marker | null>(null);
-  const customControl = new naver.maps.CustomControl('<a href="#" class="btn_mylct"><img src="/current-circle-blue.svg" width="10%" height="10%" style="margin: 0 0 22.4px 10px"></img></a>', {
+  
+  const customControl = new naver.maps.CustomControl('<button><img src="/current-circle-blue.svg" width="10%" height="10%" style="margin: 0 0 22.4px 10px"></img></button>', {
     position: naver.maps.Position.LEFT_BOTTOM
+  });
+  const locationControl = new naver.maps.CustomControl('<button><img src="/triangle.svg" width="10%" height="10%" style="margin: 0 0 22.4px 10px"></img></button>', {
+    position: naver.maps.Position.LEFT_TOP
   });
 
   const initMapSetting = () => {
@@ -40,9 +48,26 @@ const Map: React.FC<Address> = ({ route }) => {
       endIcon: 1,
     });
 
+    // setLocationMarker(() => {
+    //   console.log("locationMarker rendered!");
+
+    //   return new naver.maps.Marker({ position: new naver.maps.LatLng(0, 0), map: mapRef.current! });
+    // });
+    
+    const handleLocationMove = function () {
+      setLocation((preLocation) => ({ // 와.... location의 불변성 유지 필요....
+        ...preLocation,
+        lat: preLocation.lat+0.005,
+        lng: preLocation.lng,
+      })); // 상태 업데이트
+    };
+    naver.maps.Event.addDOMListener(locationControl.getElement(), "click", handleLocationMove);
+
     naver.maps.Event.once(mapRef.current, 'init', function () {
       customControl.setMap(mapRef.current);
+      testFlag && locationControl.setMap(mapRef.current);
     });
+
     return [mapMarker, startMapMarker, polyline];
   }
 
@@ -56,7 +81,11 @@ const Map: React.FC<Address> = ({ route }) => {
           lat: position.coords.latitude,
           lng: position.coords.longitude,
         };
-        setLocation(newLocation); // 상태 업데이트
+        setLocation((preLocation) => ({ // 와.... location의 불변성 유지 필요....
+          ...preLocation,
+          lat: newLocation.lat,
+          lng: newLocation.lng,
+        })); // 상태 업데이트
       },
       (error) => {
         console.error("Error getting location:", error.message);
@@ -71,23 +100,27 @@ const Map: React.FC<Address> = ({ route }) => {
   }, [])
 
   useEffect(() => {
+    locationRef.current = location;
+
     if (location && mapRef.current) {
       if (!markerRef.current) {
         markerRef.current = new naver.maps.Marker({
           position: location,
           map: mapRef.current,
         });
+      } else {
+        markerRef.current.setPosition(new naver.maps.LatLng(location.lat, location.lng));
       }
-    } {
-      markerRef.current?.setPosition(new naver.maps.LatLng(location.lat, location.lng));
     }
+    const handleCenterMove = function () {    
+      if (locationRef.current && mapRef.current) {
+        mapRef.current.setCenter(new naver.maps.LatLng(locationRef.current.lat, locationRef.current.lng));
+      }
+    };
+    
+    naver.maps.Event.removeDOMListener(customControl.getElement(), "click", handleCenterMove);
+    naver.maps.Event.addDOMListener(customControl.getElement(), "click", handleCenterMove);
   }, [location]);
-
-  useEffect(() => {
-    const handleCenterMove = function () { mapRef.current!.setCenter(new naver.maps.LatLng(location.lat, location.lng)); }
-    naver.maps.Event.clearListeners(customControl, "click");
-    naver.maps.Event.addDOMListener(customControl.getElement(), 'click', handleCenterMove);
-  }, [mapRef.current?.getCenter()]);
 
   return (
     <>
